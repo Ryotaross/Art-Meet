b4dc8ac203802080e2bfb81af2a928c8eb63c16f
Ryotaro Shimizu <ryotaro_625@icloud.com>
2021-10-20 14:25:26 +0900

    index,detail画面作成（仮）

--- backend/resources/js/components/detail.js
+++ backend/resources/js/components/detail.js
@@ -8,25 +8,25 @@ function Detail() {
   const BackArrow = styled.div`
     width: 16px;
     height: 19px;
-    background-color: #323a44;
   `;
 
   const Bitmap = styled.div `
     width: 390px;
     height: 295px;
     padding: 20px 366px 256px 8px;
-  `
+  `;
 
   const Rectangle6 = styled.div `
     width: 390px;
     height: 257px;
     padding: 18px 0 22px 0;
     background-color: #fff;
-  `
+  `;
+
   const Text = styled.div `
     margin-top:18px;
     margin-left:30px;
-  `
+  `;
   
   const ItemName = styled.p`
     width: 264px;
@@ -39,7 +39,7 @@ function Detail() {
     line-height: 1.08;
     letter-spacing: normal;
     color: #333;
-  `
+  `;
 
   const Maker = styled.p`
     width: 351px;
@@ -80,78 +80,9 @@ function Detail() {
     color: #040404;
   `;
 
-  const OfficialBtn = styled.button `
-    width: 105px;
-    height: 53px;
-    margin: 8.2px 5px 0 0;
-    padding: 15.7px 28.6px 16.6px 28.2px;
-    border-radius: 100px;
-    border: solid 4px #4b4f63;
-  `;
-
-  const officialText = styled.span `
-    width: 48.1px;
-    height: 20.7px;
-    font-family: YuKyo;
-    font-size: 18px;
-    font-weight: bold;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    text-align: center;
-    color: #4b4f63;
-  `;
-
-  const AmazonBtn = styled.button `
-    width: 105px;
-    height: 53px;
-    margin: 8.2px 5px 0 5px;
-    padding: 15.7px 11.8px 16.6px 11.2px;
-    border-radius: 100px;
-    border: solid 4px #eb7a23;
-  `;
-
-  const AmazonText = styled.span `
-    width: 82px;
-    height: 20.7px;
-    font-family: YuKyo;
-    font-size: 18px;
-    font-weight: bold;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    text-align: center;
-    color: #eb7a23;
-  `;
-
-  const RakutenBtn = styled.button`
-    width: 105px;
-    height: 53px;
-    margin: 8.2px 5px 0 0;
-    padding: 15.7px 28.6px 16.6px 28.2px;
-    border-radius: 100px;
-    border: solid 4px #e41919;
-  `;
-
-  const RakutenText = styled.span `
-    width: 48.1px;
-    height: 20.7px;
-    font-family: YuKyo;
-    font-size: 18px;
-    font-weight: bold;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    text-align: center;
-    color: #e41919;
-  `;
-
   return(
     <>
-      <BackArrow></BackArrow>
+      <BackArrow>＜</BackArrow>
       <Bitmap></Bitmap>
       <Rectangle6>
         <Text>
@@ -168,14 +99,14 @@ function Detail() {
           原材料：大豆、油
         </Content>
         </Text>
-        <Stack direction="row" spacing={2}>
-          <Button variant="contained" color="success">
+        <Stack direction="row" spacing={2} justifyContent="center" alignItems="center" sx={{my:5}}>
+          <Button variant="contained" color="primary">
             公式
           </Button>
-          <Button variant="contained" color="success">
+          <Button variant="contained" color="warning">
             Amazon
           </Button>
-          <Button variant="contained" color="success">
+          <Button variant="contained" color='error'>
             楽天
           </Button>
         </Stack>
